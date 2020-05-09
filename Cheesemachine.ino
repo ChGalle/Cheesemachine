246b4adbc70f4477b6a014219d2747e72f9da9c2
Christoph Galle <christoph.galle@buechnetz.de>
2020-05-09 22:09:43 +0200

    Update Cheesemachine.ino
    Kommentare hinzugefügt, verwaisten Code entfernt.

--- Cheesemachine.ino
+++ Cheesemachine.ino
@@ -1,45 +1,45 @@
+//  Mit dieser Software lässt sich ein Einkochkessel mit einem Solid-State-Relais, und einem DS18B20 Temperatursensor steuern,
+//  so dass dieser möglichst genau eine Temperatur von 32°C erreicht und hält.
+
+
 #include <DallasTemperature.h>
 #include <OneWire.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define TEMP_READ_DELAY 800 //can only read digital temp sensor every ~750ms
-#define dutycycle 8000
-#define oncycle 2000
-#define RELAY_PIN 7
-#define TEMP_PROBE_PIN 2
-#define TEMP_PROBE_GND 3
-#define TEMP_PROBE_VCC 4
+#define TEMP_READ_DELAY 800 //der Sensor kann nur ca. alle 750ms einen Messwert liefern
+#define dutycycle 400 // Ruhezeit im Pulsebetrieb
+#define oncycle 100 // Einschaltzeit im Pulsbetrieb
+#define RELAY_PIN 7 //Relaispin
+#define TEMP_PROBE_PIN 2 // DS18B20 DataPin
+#define TEMP_PROBE_GND 3 // DS18B20 GND PIN
+#define TEMP_PROBE_VCC 4 // DS18B20 VCC PIN
 
-double setpoint = 32;
-double temperature;
+double setpoint = 32; // Gewünschte Zieltemperatur
+double temperature; // Variable für die Temperatur
 int relayMode = 0; //Aus = 0, Halb = 1, Ganz = 2
 
-bool dutystate;
-bool relayState;
-
+// Initialisiere die Bibliotheken
 OneWire oneWire(TEMP_PROBE_PIN);
 DallasTemperature temperatureSensors(&oneWire);
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+LiquidCrystal_I2C lcd(0x27, 20, 4); //LCD mit 20x4 Zeichen
 
-unsigned long lastTempUpdate; //tracks clock time of last temp update
-unsigned long lastRelayUpdate; //tracks clock time of last temp update
+unsigned long lastTempUpdate; //Letztes Temperaturupdate
 
 
 bool updateTemperature() {
   if ((millis() - lastTempUpdate) > TEMP_READ_DELAY) {
-    temperature = temperatureSensors.getTempCByIndex(0); //get temp reading
+    temperature = temperatureSensors.getTempCByIndex(0); //hole Messung ab
     lastTempUpdate = millis();
-    temperatureSensors.requestTemperatures(); //request reading for next time
-    Serial.println(temperature);
+    temperatureSensors.requestTemperatures(); //Beauftrage nächste Messung
     return true;
   }
   return false;
-}//void updateTemperature
+}
 
 void setup() {
-  lcd.init();                      // initialize the lcd
-  lcd.backlight();
+  lcd.init();                      // Initialisiere LCD
+  lcd.backlight();                 // Schalte LCD Hintergrundbeleuchtung ein
   lcd.home();
   lcd.print("Cheesy-Peasy-Machine");
   lcd.setCursor(0, 2);
@@ -56,18 +56,18 @@ void setup() {
 
   temperatureSensors.begin();
   temperatureSensors.requestTemperatures();
-  while (!updateTemperature()) {} //wait until temp sensor updated
+  while (!updateTemperature()) {} //Warte bis erste Temperatur vorliegt...
 }
 
 void heizbestimmung() {
   if ((setpoint - temperature) >= 10.0 ) {
-    //Do something
+    //Volle Heizleistung
     relayMode = 2;
   } else if ((setpoint - temperature) >= 1.0) {
-    //Do something
+    //Reduzierte Heizleistung
     relayMode = 1;
   } else {
-    //Do something
+    //Beende Heizen
     relayMode = 0;
   }
 }
@@ -75,9 +75,9 @@ void heizbestimmung() {
 void relayCycle() {
   if (relayMode == 1) {
     digitalWrite(RELAY_PIN, HIGH);
-    delay(100);
+    delay(oncycle);
     digitalWrite(RELAY_PIN, LOW);
-    delay(400);
+    delay(dutycycle);
 
   }   else if (relayMode == 0) {
     //Do something
@@ -90,18 +90,18 @@ void relayCycle() {
 void lcdAusgabe() {
   lcd.setCursor(0, 0);
   lcd.print("Aktuelle Temperatur:");
-  
+
   if (temperature >= 10) {
     lcd.setCursor(0, 1);
     lcd.print(temperature);
-  } else if (temperature >= 0){
+  } else if (temperature >= 0) {
     lcd.setCursor(0, 1);
     lcd.print(" ");
     lcd.print(temperature);
-  } else if (temperature == -127){
+  } else if (temperature == -127) {
     lcd.setCursor(0, 1);
     lcd.print("ERR    ");
-    
+
   }
 
 }
